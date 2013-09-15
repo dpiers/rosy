680086e2b70b14ceee771215319bc8571e1cd8b5
Daniel Piers <danielpiers@gmail.com>
2013-09-14 22:20:56 -0700

    create go server

---
+++ rosy.go
@@ -0,0 +1,101 @@
+/*
+	Rosy the Riveting Code Evaluator
+	Copyright 2013, Team Piers-Rollenhagen
+
+*/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/ant0ine/go-json-rest"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+// Basic error handler
+func errHndlr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	restHandler := rest.ResourceHandler{
+		EnableRelaxedContentType: true,
+		DisableJsonIndent:        false,
+	}
+
+	restHandler.SetRoutes(
+		rest.Route{"POST", "/cpp", EvalCpp},
+		rest.Route{"POST", "/go", EvalGo},
+		rest.Route{"POST", "/haskell", EvalHaskell},
+		rest.Route{"POST", "/python", EvalPython},
+		rest.Route{"POST", "/ruby", EvalRuby},
+	)
+
+	http.ListenAndServe(":9000", &restHandler)
+}
+
+func executeWithDocker(command string, w *rest.ResponseWriter) {
+	cmd := exec.Command("sudo", "docker", "run", command)
+	stdout, err := cmd.StdoutPipe()
+	errHndlr(err)
+	stderr, err := cmd.StderrPipe()
+	errHndlr(err)
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	buf := bytes.NewBuffer(nil)
+	buf.ReadFrom(stderr)
+	buf.ReadFrom(stdout)
+
+	w.Write(buf.Bytes())
+}
+
+func EvalCpp(w *rest.ResponseWriter, r *rest.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	w.Write([]byte("eval cpp...\n"))
+}
+
+func EvalGo(w *rest.ResponseWriter, r *rest.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	w.Write([]byte("eval go...\n"))
+
+}
+
+func EvalHaskell(w *rest.ResponseWriter, r *rest.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	input := r.FormValue("input")
+	command := fmt.Sprintf("echo %s > h.hs; runhaskell h.hs", input)
+	executeWithDocker(command, w)
+}
+
+func EvalPython(w *rest.ResponseWriter, r *rest.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	w.Write([]byte("eval python...\n"))
+}
+
+func EvalRuby(w *rest.ResponseWriter, r *rest.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+	w.Write([]byte("eval ruby...\n"))
+}
