5d82388c6bb622c04d0266a5f3debfb37d68562e
Daniel Piers <danielpiers@gmail.com>
2013-09-15 02:13:05 -0700

    poor rosy just needs the right encoding

--- rosy.go
+++ rosy.go
@@ -12,7 +12,6 @@ import (
 	"github.com/ant0ine/go-json-rest"
 	"log"
 	"net/http"
-	"net/url"
 	"os/exec"
 )
 
@@ -74,19 +73,14 @@ func EvalCpp(w *rest.ResponseWriter, r *rest.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-	input := r.FormValue("input")
-	fmt.Println(input)
-	input, err := url.QueryUnescape(input)
-	errHndlr(err)
-	fmt.Println(input)
-
+	input := fmt.Sprintf("%q", r.FormValue("input"))
 	commands := []string{
 		"docker",
 		"run",
 		"rosy/multilingual",
 		"sh",
 		"-c",
-		"echo " + input + " > c.cpp; g++ c.cpp > a.out; ./a.out",
+		"echo -e" + input + " > c.cpp; g++ c.cpp > a.out; ./a.out",
 	}
 	executeWithSudo(commands, w)
 }
@@ -96,18 +90,14 @@ func EvalGo(w *rest.ResponseWriter, r *rest.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-	input := r.FormValue("input")
-	fmt.Println(input)
-	input = fmt.Sprintf("%q", r.FormValue("input"))
-	fmt.Println(input)
-
+	input := fmt.Sprintf("%q", r.FormValue("input"))
 	commands := []string{
 		"docker",
 		"run",
 		"rosy/multilingual",
 		"sh",
 		"-c",
-		"echo " + input + " > g.go; go run g.go",
+		"echo -e" + input + " > g.go; go run g.go",
 	}
 	executeWithSudo(commands, w)
 
@@ -118,17 +108,14 @@ func EvalHaskell(w *rest.ResponseWriter, r *rest.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-	input := r.FormValue("input")
-	fmt.Println(input)
-	input = fmt.Sprintf("%q", r.FormValue("input"))
-	fmt.Println(input)
+	input := fmt.Sprintf("%q", r.FormValue("input"))
 	commands := []string{
 		"docker",
 		"run",
 		"rosy/multilingual",
 		"sh",
 		"-c",
-		"echo " + input + " > h.hs; runhaskell h.hs",
+		"echo -e" + input + " > h.hs; runhaskell h.hs",
 	}
 	executeWithSudo(commands, w)
 }
@@ -138,17 +125,14 @@ func EvalJavaScript(w *rest.ResponseWriter, r *rest.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-	input := r.FormValue("input")
-	fmt.Println(input)
-	input = fmt.Sprintf("%q", r.FormValue("input"))
-	fmt.Println(input)
+	input := fmt.Sprintf("%q", r.FormValue("input"))
 	commands := []string{
 		"docker",
 		"run",
 		"rosy/multilingual",
 		"sh",
 		"-c",
-		"echo " + input + " > j.js; rhino j.js",
+		"echo -e" + input + " > j.js; rhino j.js",
 	}
 	executeWithSudo(commands, w)
 
@@ -159,17 +143,14 @@ func EvalPython(w *rest.ResponseWriter, r *rest.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-	input := r.FormValue("input")
-	fmt.Println(input)
-	input = fmt.Sprintf("%q", r.FormValue("input"))
-	fmt.Println(input)
+	input := fmt.Sprintf("%q", r.FormValue("input"))
 	commands := []string{
 		"docker",
 		"run",
 		"rosy/multilingual",
 		"sh",
 		"-c",
-		"echo " + input + " > p.py; python p.py",
+		"echo -e" + input + " > p.py; python p.py",
 	}
 	executeWithSudo(commands, w)
 }
@@ -179,17 +160,14 @@ func EvalRuby(w *rest.ResponseWriter, r *rest.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-	input := r.FormValue("input")
-	fmt.Println(input)
-	input = fmt.Sprintf("%q", r.FormValue("input"))
-	fmt.Println(input)
+	input := fmt.Sprintf("%q", r.FormValue("input"))
 	commands := []string{
 		"docker",
 		"run",
 		"rosy/multilingual",
 		"sh",
 		"-c",
-		"echo " + input + " > r.rb; ruby r.rb",
+		"echo -e" + input + " > r.rb; ruby r.rb",
 	}
 	executeWithSudo(commands, w)
 }
