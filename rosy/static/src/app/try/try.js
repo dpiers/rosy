175c2c6854360f2420bc5fcfe2643941610377d3
Jim Rollenhagen <jim@jimrollenhagen.com>
2013-09-14 21:29:59 -0700

    urls are strings not objects

--- rosy/static/src/app/try/try.js
+++ rosy/static/src/app/try/try.js
@@ -32,7 +32,7 @@ angular.module( 'rosy.try', [
   };
 
   $scope.runCode = function(code, language) {
-    $http.post('http://tryrosy.com/' + language, {code: code}).
+    $http.post('http://tryrosy.com/' + language.name.toLowerCase(), {code: code}).
       success(function(data) {
         console.log(data);
       });
